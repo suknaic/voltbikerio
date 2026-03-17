d056e01f882cf1a9cbf9c13104305a9ca67e9928
Felipe Suknaic <fsuknaic@hotmail.com>
2026-03-16 22:51:47 -0500

    feat: update logo and branding in app layout components

--- resources/js/layouts/auth/auth-simple-layout.tsx
+++ resources/js/layouts/auth/auth-simple-layout.tsx
@@ -17,9 +17,8 @@ export default function AuthSimpleLayout({
                             href={home()}
                             className="flex flex-col items-center gap-2 font-medium"
                         >
-                            <div className="mb-1 flex h-9 w-9 items-center justify-center rounded-md">
-                                {/* <AppLogoIcon className="size-9 fill-current text-[var(--foreground)] dark:text-white" /> */}
-                                <img src="public/assets/logo-site.png" height={100} width={100} alt="" />
+                            <div className="mb-1 flex items-center justify-center">
+                                <img src="/assets/logo-site.png" height={80} width={80} alt="Logo" className="object-contain" />
                             </div>
                             <span className="sr-only">{title}</span>
                         </Link>
