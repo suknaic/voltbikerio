824b696b6372dfb8720a2e99e78222b0cb38893c
Felipe Suknaic <fsuknaic@hotmail.com>
2026-05-12 11:28:40 -0500

    feat: atualizar a rota de logout para usar a função de logout dinâmica

--- resources/js/components/user-menu-content.tsx
+++ resources/js/components/user-menu-content.tsx
@@ -8,6 +8,7 @@ import {
 } from '@/components/ui/dropdown-menu';
 import { UserInfo } from '@/components/user-info';
 import { useMobileNavigation } from '@/hooks/use-mobile-navigation';
+import { logout,  } from '@/routes';
 import type { User } from '@/types';
 
 type Props = {
@@ -47,7 +48,7 @@ export function UserMenuContent({ user }: Props) {
             <DropdownMenuItem asChild>
                 <Link
                     className="block w-full cursor-pointer"
-                    href="/logout"
+                    href={logout()}
                     as="button"
                     onClick={handleLogout}
                     data-test="logout-button"
